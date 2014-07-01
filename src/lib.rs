0a981b3f7bc1581edaa6855ea1d0efff35cfa737
zzmp <zmp@umich.edu>
2014-06-30 21:13:07 -0700

    (doc) Added doc attributes.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![doc(html_logo_url = "https://avatars0.githubusercontent.com/u/7853871?s=128", html_favicon_url = "https://avatars0.githubusercontent.com/u/7853871?s=256", html_root_url = "http://ironframework.io/core/cookie")]
+#![doc(html_logo_url = "https://avatars0.githubusercontent.com/u/7853871?s=128", html_favicon_url = "https://avatars0.githubusercontent.com/u/7853871?s=256", html_root_url = "http://ironframework.io/core/cookie")]
 #![crate_id = "cookie"]
 #![deny(missing_doc)]
 #![feature(phase)]
