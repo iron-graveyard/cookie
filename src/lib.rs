e13ee4569965a8d5be1f210d967dbe8bc23064f7
theptrk <patrick.tran06@gmail.com>
2014-07-07 23:08:06 -0700

    (fix) Changed to crate name

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![doc(html_logo_url = "https://avatars0.githubusercontent.com/u/7853871?s=128", html_favicon_url = "https://avatars0.githubusercontent.com/u/7853871?s=256", html_root_url = "http://ironframework.io/core/cookie")]
 #![doc(html_logo_url = "https://avatars0.githubusercontent.com/u/7853871?s=128", html_favicon_url = "https://avatars0.githubusercontent.com/u/7853871?s=256", html_root_url = "http://ironframework.io/core/cookie")]
-#![crate_id = "cookie"]
+#![crate_name = "cookie"]
 #![deny(missing_doc)]
 #![feature(phase)]
 #![feature(globs)]
